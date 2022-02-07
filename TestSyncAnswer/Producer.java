66c5875d6c1683d55ee8f544aea23cc4fa8f3361
mikevandy <mvander4@students.kennesaw.edu>
2022-02-06 20:05:14 -0500

    TestSync Answer Modifications

--- TestSyncAnswer/Producer.java
+++ TestSyncAnswer/Producer.java
@@ -1,4 +1,5 @@
-
+//Problem requires display to vary
+//Introduced Collections.shuffle() method to shuffle the items in the messages list
 
 import java.util.*;
 import java.util.concurrent.*;
@@ -24,7 +25,7 @@ class Producer
         try
         {
 	
-		Collections.shuffle(messages);
+		Collections.shuffle(messages); //Added Collection.shuffle method to change display
         	for (String s : messages)
                 drop.put(s);
             drop.put("DONE");
--- TestSyncAnswer/SynQApp.java
+++ TestSyncAnswer/SynQApp.java
@@ -1,55 +1,8 @@
+//Removed Producer and Consumer Class and created separate files
+
 import java.util.*;
 import java.util.concurrent.*;
 
-class Producer
-    implements Runnable
-{
-    private BlockingQueue<String> drop;
-    List<String> messages = Arrays.asList(
-        "Mares eat oats",
-        "Does eat oats",
-        "Little lambs eat ivy",
-        "Wouldn't you eat ivy too?");
-        
-    public Producer(BlockingQueue<String> d) { this.drop = d; }
-    
-    public void run()
-    {
-        try
-        {
-            for (String s : messages)
-                drop.put(s);
-            drop.put("DONE");
-        }
-        catch (InterruptedException intEx)
-        {
-            System.out.println("Interrupted! " + 
-                "Last one out, turn out the lights!");
-        }
-    }    
-}
-
-class Consumer
-    implements Runnable
-{
-    private BlockingQueue<String> drop;
-    public Consumer(BlockingQueue<String> d) { this.drop = d; }
-    
-    public void run()
-    {
-        try
-        {
-            String msg = null;
-            while (!((msg = drop.take()).equals("DONE")))
-                System.out.println(msg);
-        }
-        catch (InterruptedException intEx)
-        {
-            System.out.println("Interrupted! " + 
-                "Last one out, turn out the lights!");
-        }
-    }
-}
 
 public class SynQApp
 {
